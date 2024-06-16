458b5b16dbd33165332d115545df19c176d8e61a
Christian González <chrisgonzaco@gmail.com>
2024-06-16 21:38:37 +0200

    removed unused modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown'
-
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-article.netlify.app/',
 	integrations: [
     tailwind(),
-		mdx(), 
-		sitemap(),
 		partytown({
 			config: {
 			  forward: ["dataLayer.push"],
