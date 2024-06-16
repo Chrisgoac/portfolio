dc373afbfb8d10a1363b87b21067c3017350f5f2
Christian González <chrisgonzaco@gmail.com>
2024-06-16 21:36:37 +0200

    added google analytics and improved website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	site: 'https://astro-article.netlify.app/',
+	integrations: [
+    tailwind(),
+		mdx(), 
+		sitemap(),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+	],
 });
